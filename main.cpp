8414219debed64a876911262f1d056428c15fc22
SSGSSACE <quangbayhai@gmail.com>
2021-06-19 00:32:25 +0700

    June 19, 2021 12:32 AM

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     int a=200;
     long y= long(a);
     int s;
-    cout<<"X=: "<<endl;
+    cout<<"X=: "<<y<<endl;
     cout << "NNNN" << endl;
     cout << "Hello" << endl;
     return 0;
