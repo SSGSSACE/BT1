f0b09c749b25bfa64986e1e7127e0c8f7fb4d13f
SSGSSACE <quangbayhai@gmail.com>
2021-06-18 23:42:17 +0700

    June 18, 2021 11:42 PM

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int b=5;
+    int s;
+    cout<<s<<
+    cout << "NNNN" << endl;
+    cout << "Hello" << endl;
+    return 0;
+}
